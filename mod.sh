04eaf2c96e659afd84a2bad68d962b6ec9f2ca10
andigoenfx <74978790+andigoenfx@users.noreply.github.com>
2021-07-17 20:00:03 +0700

    Create mod.sh

---
+++ mod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+opkg update && opkg install curl
+wget --no-check-certificate "https://raw.githubusercontent.com/andigoenfx/BOT-TELEGRAM-OPENWRT/main/functions/pulpstone_general.sh" -O /root/pulpstone_bot/functions/pulpstone_general.sh
+wget --no-check-certificate "https://raw.githubusercontent.com/andigoenfx/BOT-TELEGRAM-OPENWRT/main/functions/xderm-trigger.sh" -O /root/pulpstone_bot/functions/xderm-trigger.sh
+wget --no-check-certificate "https://raw.githubusercontent.com/andigoenfx/BOT-TELEGRAM-OPENWRT/main/pulpstone_telegram_bot" -O /root/pulpstone_bot/pulpstone_telegram_bot
+chmod +x /root/pulpstone_bot/functions/xderm-trigger.sh
+chmod +x /root/pulpstone_bot/functions/pulpstone_general.sh
+chmod +x /root/pulpstone_bot/pulpstone_telegram_bot
+sleep 2
+echo "install selesai"
+echo "router akan reboot terlebih dahulu"
+reboot
